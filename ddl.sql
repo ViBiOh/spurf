471148b3e201e4249bad055ce8f062fd48477b40
Vincent Boutour <bob@vibioh.fr>
2020-05-25 22:31:48 +0200

    refactor(db): Moving table and data into their own schema

--- ddl.sql
+++ ddl.sql
@@ -2,10 +2,15 @@
 DROP TABLE IF EXISTS enedis_value;
 DROP INDEX IF EXISTS enedis_value_ts;
 
+DROP SCHEMA IF EXISTS spurf;
+
+-- schema
+CREATE SCHEMA spurf;
+
 -- enedis_value
-CREATE TABLE enedis_value (
+CREATE TABLE spurf.enedis_value (
   ts TIMESTAMP WITH TIME ZONE NOT NULL,
   value REAL NOT NULL
 );
 
-CREATE UNIQUE INDEX enedis_value_ts ON enedis_value (ts);
+CREATE UNIQUE INDEX enedis_value_ts ON spurf.enedis_value(ts);
---
+++ migration_2020-05-25_1.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA spurf;
+
+ALTER TABLE enedis_value SET SCHEMA spurf;
