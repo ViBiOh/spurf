6d0622058ce3ff0e82a54691672d8eeb4a0568f2
Vincent Boutour <bob@vibioh.fr>
2020-07-18 13:53:50 +0200

    feat: Removing enedis parse of ui, relying only on file upload

--- pkg/enedis/model.go
+++ pkg/enedis/model.go
@@ -1,26 +1,5 @@
 package enedis
 
-var (
-	emptyConsumption = Consumption{}
-)
-
-// Consumption describes consumption response
-type Consumption struct {
-	Etat   Etat
-	Graphe Graphe
-}
-
-// Etat describes status of output
-type Etat struct {
-	Valeur     string
-	ErreurText string
-}
-
-// Graphe describes graphical data point
-type Graphe struct {
-	Data []Value
-}
-
 // Value describes data point
 type Value struct {
 	Valeur    float64
