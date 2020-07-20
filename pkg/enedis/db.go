aef6588a46bedcf5b8122c16fe9530be299eddc8
Vincent Boutour <bob@vibioh.fr>
2020-07-20 12:00:26 +0200

    refactor: Improving bulk load by reducing memory footprint

--- pkg/enedis/db.go
+++ pkg/enedis/db.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/ViBiOh/httputils/v3/pkg/db"
-	"github.com/ViBiOh/httputils/v3/pkg/logger"
 )
 
 const lastFetch = `
@@ -28,23 +27,8 @@ func (a app) getLastFetch(ctx context.Context) (time.Time, error) {
 	return output, db.Get(ctx, a.db, scanner, lastFetch, a.name)
 }
 
-func (a app) save(ctx context.Context, datas []Value) error {
-	logger.Info("Saving %d records", len(datas))
-
+func (a app) save(ctx context.Context, feeder func(stmt *sql.Stmt) error) error {
 	return db.DoAtomic(ctx, a.db, func(ctx context.Context) error {
-		var index int
-		feeder := func(stmt *sql.Stmt) error {
-			if index == len(datas) {
-				return db.ErrBulkEnded
-			}
-
-			data := datas[index]
-			index++
-
-			_, err := stmt.Exec(a.name, data.Timestamp, data.Valeur)
-			return err
-		}
-
 		return db.Bulk(ctx, feeder, "spurf", "enedis_value", "name", "ts", "value")
 	})
 }
